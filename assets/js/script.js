f654d8e89b3e282416e1c0c6d0e1042fe67e8838
TJones305 <tim.w.j@hotmail.com>
2021-03-15 11:39:03 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+
+/*Required Functions: 
+* onClick Functions will caused Goolge Maps to display Modern Day counties were Old England Regions use to be.
+*timelineSlide will cause information about selected historical event to appear under the slider. 
+ */
+
+//onClickGoolgeMapsEast()
+//onClickGoolgeMapsMercia()
+//onClickGoolgeMapsNorth()
+//onClickGoolgeMapsWessex()
+//timelineSlide()
+
+/* Potentially not required Functions:
+*Unsure whether onClickTextSlide functions are needed at this stage, as it appears this fintionality is part of the accordion 
+feature, although they were initially thought required. 
+Functionality will cause accordion text to expand for selected region and close other regions*/
+
+//onClickTextSlideEast()
+//onClickTextSlideMercia()
+//onClickTextSlideNorth()
+//onClickTextSlideWessex()
