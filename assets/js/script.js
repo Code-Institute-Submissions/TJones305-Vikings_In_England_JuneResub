235cf4ff4f8c8b0a2f78abc44a38efae0cedc74b
TJones305 <tim.w.j@hotmail.com>
2021-03-17 17:53:24 +0000

    added Google Maps JS and applied CSS styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,16 @@
 * onClick Functions will caused Goolge Maps to display Modern Day counties were Old England Regions use to be.
 *timelineSlide will cause information about selected historical event to appear under the slider. 
  */
-
+         function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 6,
+                center: {
+                    lat: 52.3124563,
+                    lng: -0.079274
+                }
+            });
+        }
+//54.2288534,-4.2051138,5.75
 //onClickGoolgeMapsEast()
 //onClickGoolgeMapsMercia()
 //onClickGoolgeMapsNorth()
@@ -23,4 +32,4 @@ Functionality will cause accordion text to expand for selected region and close 
 /*Google Maps:
 *Requires zoom on UK/England, Cluster funtionality needed to highlight required counties when onClickGoogleMaps funtion
 is activated.
-*/
+        */
