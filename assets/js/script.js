a86ad0da644e8562ed641622afd224bb87291b35
TJones305 <tim.w.j@hotmail.com>
2021-03-19 15:48:39 +0000

    updated co-ordinates on Goolge Maps to county centres

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ is activated.
 function initMap() {
   const myLatLng = { lat: 52.3124563, lng: -0.079274 };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 7,
+    zoom: 7.5,
     center: myLatLng,
   });
   
@@ -23,89 +23,91 @@ function initMap() {
             label: labels[i % labels.length]
             });
         });
-  let markerCluster = new MarkerClusterer(map, markers,);
+  let markerCluster = new MarkerClusterer(map, markers,{
+      imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
   }
 
   let locations = [
-//Suffolk
-     {lat:52.2358344, lng:-0.0666801},
+/*East Anglia*/
+ //Suffolk
+    {lat:52.1555477, lng:0.8305162},
 //Norfolk
-    {lat:52.6687466, lng:-0.171606},
+    {lat:52.6458602, lng:1.1001733},
+/*Mercia*/
 //Hertfordshire: 
-    {lat:51.8347302, lng:-1.3962391},
+    {lat:51.8024912, lng:-0.2273691},
 //Northamptonshire:
-	{lat:52.305134,	lng:-1.9580972},
+	{lat:52.3326223, lng:-0.8612687},
 //Gloucestershire:
-	{lat:51.8397288, lng:-3.2724975},
+	{lat:51.8812293, lng:-2.1034647},
 //Shropshire: 
-	{lat:52.6470409, lng:-3.8553464},
+	{lat:52.6422402, lng:-2.6755677},
 //Warwickshire:
-	{lat:52.3199926, lng:-2.1275127},
+	{lat:52.2605613, lng:-1.5047367},
 //Oxfordshire:
-	{lat:51.8126104, lng:-1.8552331},
+	{lat:51.7846263, lng:-1.3165567},
 //Cambridgeshire:
-	{lat:52.3675758, lng:-1.1138455},
+	{lat:52.8558072, lng:-2.0336547},
 //Staffordshire: 
-	{lat:52.8194524, lng:-3.149266},
+	{lat:52.3161863, lng:0.0763663},
 //Cheshire:
-	{lat:53.2087709, lng:-3.672641},
+	{lat:53.2058542, lng:-2.3560777},
 //Derbyshire:
-	{lat:53.1171519, lng:-2.1607295},
+	{lat:53.0718692, lng:-1.5065807},
 //Leicestershire: 
-	{lat:52.6796183, lng:-2.251943},
+	{lat:52.6459512, lng:-1.1054757},
 //Herefordshire:
-	{lat:52.1093796, lng:-3.3003807},
+	{lat:52.0949103, lng:-2.6791307},
 //Nottinghamshire:
-	{lat:53.1446192, lng:-1.565864},
+	{lat:53.1314412, lng:-0.9775317},
 //Lincolnshire:
-	{lat:53.1230266, lng:-1.35247},
+	{lat:53.1253312, lng:-0.1814947},
 //Rutland:
-	{lat:52.6383279, lng:-0.8256757},
+	{lat:52.6572112, lng:-0.6478657},
 //Bedfordshire:
-	{lat:52.0626837, lng:-0.9835095},
+	{lat:52.0671493, lng:-0.4650577},
 //Buckinghamshire:
-	{lat:51.8395709, lng:-1.3690974},
+	{lat:51.7817283, lng:-0.7855967},
 //Berkshire:
-	{lat:51.4480557, lng:-2.1602006},
+    {lat:51.4045883, lng:-1.0161207},
+/*Northumbria*/
 //Lancashire: 
-	{lat:53.8559373, lng:-3.6859045},
+	{lat:53.8671211, lng:-2.6202687},
 //Yorkshire:
-	{lat:53.9109609, lng:-3.4503445},
+	{lat:53.9761501, lng:-1.1075017},
 //Northumberland:
-	{lat:55.2918812, lng:-3.1961203},
+	{lat:55.20421, lng:-2.0282457},
 //Durham:
-	{lat:54.7817348, lng:-1.622277},
+	{lat:54.7109391, lng:-1.7808717},
 //Cumbria:
-	{lat:54.6091293, lng:-4.0209014},
+    {lat:54.6007491, lng:-2.8466487},
+/*wessex*/
 //Kent: 
-	{lat:51.191039,	lng:-0.3784946},
+	{lat:51.2108663, lng:0.7759013},
 //Cornwall:
-	{lat:50.4379558, lng:-6.0771768},
+	{lat:50.4087534, lng:-4.8320077},
 //Sussex:
 	{lat:50.933657,	lng:-0.212399},
 //Surrey: 
-	{lat:51.2661606, lng:-1.5164938},
+	{lat:51.2608723, lng:-0.4978297},
 //Hampshire:
-	{lat:51.0394658, lng:-2.4644518},
+	{lat:51.1042083, lng:-1.2324547},
 //Somerset:
-	{lat:51.0715592, lng:-4.1632592},
+	{lat:51.0893883, lng:-2.8357007},
 //Devon:
-	{lat:50.7026262, lng:-6.0267974},
+	{lat:50.7888394, lng:-3.7310407},
 //Dorset:
-    {lat:50.7915572, lng:-3.4427742},
+    {lat:50.8112134, lng:-2.2800067},
 //Wiltshire:
-	{lat:51.318715,	lng:-3.0467989},
+	{lat:51.3069553, lng:-1.9239737},
 //Essex:
-	{lat:51.7913785, lng:-0.4827174},
+	{lat:51.8208543, lng:0.5536933},
 //London
-    {lat:51.5273066, lng:-0.6620407}
+    {lat:51.5268959, lng:-0.3394719}
 ];
-      
+     
+    
 /* ---- Timeline --- */
-//onClickGoolgeMapsEast()
-//onClickGoolgeMapsMercia()
-//onClickGoolgeMapsNorth()
-//onClickGoolgeMapsWessex()
 //timelineSlide()
 
 /* Potentially not required Functions:
