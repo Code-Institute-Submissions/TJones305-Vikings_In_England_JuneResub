be1bbd94d05f00057459eec432d5616750078372
TJones305 <tim.w.j@hotmail.com>
2021-03-31 10:16:58 +0000

    added site feedback form and made cosmetic changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,8 +33,65 @@ function initMap() {
     });
     setMarkers(map);
 }
+     const markers = [
+      ["Suffolk", 52.1555477, 0.8305162, 1, "assets/images/eastanglia-icon.png"],
+      ["Norfolk", 52.6458602, 1.1001733, 2, "assets/images/eastanglia-icon.png"],
+      ["Lancashire", 53.8671211,-2.6202687, 3, "assets/images/northumbria-icon.png"],
+      ["Yorkshire", 53.9761501,-1.1075017, 4, "assets/images/northumbria-icon.png"],
+      ["Northumberland", 55.20421, -2.0282457, 5, "assets/images/northumbria-icon.png"],
+      ["Durham", 54.7109391, -1.7808717, 6, "assets/images/northumbria-icon.png"],
+      ["Cumbria", 54.6007491, -2.8466487, 7, "assets/images/northumbria-icon.png"],
+      ["Kent", 51.2108663, 0.7759013, 8, "assets/images/wessex-icon.png"],
+      ["Cornwall", 50.4087534, -4.8320077, 9, "assets/images/wessex-icon.png"],
+      ["Sussex", 50.933657,	-0.212399, 10, "assets/images/wessex-icon.png"],
+      ["Surrey", 51.2608723, -0.4978297, 11, "assets/images/wessex-icon.png"],
+      ["Hampshire", 51.1042083, -1.2324547, 12, "assets/images/wessex-icon.png"],
+      ["Somerset", 51.0893883, -2.8357007, 13, "assets/images/wessex-icon.png"],
+      ["Devon", 50.7888394, -3.7310407, 14, "assets/images/wessex-icon.png"],
+      ["Dorset", 50.8112134, -2.2800067, 15, "assets/images/wessex-icon.png"],
+      ["Wiltshire", 51.3069553, -1.9239737, 16, "assets/images/wessex-icon.png"],
+      ["Essex", 51.8208543, 0.5536933, 17, "assets/images/wessex-icon.png"],
+      ["London", 51.5268959, -0.3394719, 18, "assets/images/wessex-icon.png"],
+      ["Hertfordshire", 51.8024912, -0.2273691, 19, "assets/images/mercia-icon.png"],
+      ["Northamptonshire", 52.3326223, -0.8612687, 20, "assets/images/mercia-icon.png"],
+      ["Gloucestershire", 51.8812293, -2.1034647, 21, "assets/images/mercia-icon.png"],
+      ["Shropshire", 52.6422402, -2.6755677, 22, "assets/images/mercia-icon.png"],
+      ["Warwickshire", 52.2605613, -1.5047367, 23, "assets/images/mercia-icon.png"],
+      ["Oxfordshire", 51.7846263, -1.3165567, 24, "assets/images/mercia-icon.png"],
+      ["Staffordshire", 52.8558072, -2.0336547, 25, "assets/images/mercia-icon.png"],
+      ["Cambridgeshire", 52.3161863, 0.0763663, 26, "assets/images/mercia-icon.png"],
+      ["Cheshire", 53.2058542, -2.3560777, 27, "assets/images/mercia-icon.png"],
+      ["Derbyshire", 53.0718692, -1.5065807, 28, "assets/images/mercia-icon.png"],
+      ["Leicestershire", 52.6459512, -1.1054757, 29, "assets/images/mercia-icon.png"],
+      ["Herefordshire", 52.0949103, -2.6791307, 30, "assets/images/mercia-icon.png"],
+      ["Nottinghamshire", 53.1314412, -0.9775317, 31, "assets/images/mercia-icon.png"],
+      ["Lincolnshire", 53.1253312, -0.1814947,32, "assets/images/mercia-icon.png"],
+      ["Rutland", 52.6572112, -0.6478657, 33, "assets/images/mercia-icon.png"], 
+      ["Bedfordshire", 52.0671493, -0.4650577, 34, "assets/images/mercia-icon.png"],
+      ["Buckinghamshire", 51.7817283, -0.7855967, 35, "assets/images/mercia-icon.png"],
+      ["Berkshire", 51.4045883, -1.0161207, 36, "assets/images/mercia-icon.png"],
+    ];
 
-//combine to one function, use if statement to differentiate colours
+function setMarkers(map) {
+  const shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: "poly",
+  };
+
+  for (let i = 0; i < markers.length; i++) {
+    const marker = markers[i];
+    new google.maps.Marker({
+      position: { lat: marker[1], lng: marker[2] },
+      map,
+      icon: marker[4],
+      shape: shape,
+      title: marker[0],
+      zIndex: marker[3],
+    });
+}
+}; 
+
+//changes color on accordion buttons when mouseenters and reverts when mouseleaves
 function colorChange(event) {
     if (event.type === 'mouseenter') {
         let eastAnglia = document.getElementById("eastAnglia");
@@ -62,7 +119,6 @@ function colorChange(event) {
         const images = ["#imgHold", "#tl-img1", "#tl-img2", "#tl-img3", "#tl-img4", "#tl-img5", "#tl-img6", "#tl-img7", "#tl-img8", "#tl-img9", "#tl-img10", "#tl-img11", "#tl-img12"];
         $(images.join(', ')).css("display", "none");
     };
-
 
     function fillTimeline(clicked_id) {
         switch (clicked_id) {
@@ -142,66 +198,25 @@ function colorChange(event) {
             default:
                 clicked_id === "reset";
                 document.getElementById("timeline-heading").innerText = "";
-                document.getElementById("timeline-para").innerText = "Please pick a year from above";
+                document.getElementById("timeline-para").innerText = "You've reset the timeline! Pick a year from above or admire the Viking Compass!";
                 hideAll();
                 document.getElementById("imgHold").style.display = "block";
         }
     };
     
-        const markers = [
-      ["Suffolk", 52.1555477, 0.8305162, 1, "assets/images/eastanglia-icon.png"],
-      ["Norfolk", 52.6458602, 1.1001733, 2, "assets/images/eastanglia-icon.png"],
-      ["Lancashire", 53.8671211,-2.6202687, 3, "assets/images/northumbria-icon.png"],
-      ["Yorkshire", 53.9761501,-1.1075017, 4, "assets/images/northumbria-icon.png"],
-      ["Northumberland", 55.20421, -2.0282457, 5, "assets/images/northumbria-icon.png"],
-      ["Durham", 54.7109391, -1.7808717, 6, "assets/images/northumbria-icon.png"],
-      ["Cumbria", 54.6007491, -2.8466487, 7, "assets/images/northumbria-icon.png"],
-      ["Kent", 51.2108663, 0.7759013, 8, "assets/images/wessex-icon.png"],
-      ["Cornwall", 50.4087534, -4.8320077, 9, "assets/images/wessex-icon.png"],
-      ["Sussex", 50.933657,	-0.212399, 10, "assets/images/wessex-icon.png"],
-      ["Surrey", 51.2608723, -0.4978297, 11, "assets/images/wessex-icon.png"],
-      ["Hampshire", 51.1042083, -1.2324547, 12, "assets/images/wessex-icon.png"],
-      ["Somerset", 51.0893883, -2.8357007, 13, "assets/images/wessex-icon.png"],
-      ["Devon", 50.7888394, -3.7310407, 14, "assets/images/wessex-icon.png"],
-      ["Dorset", 50.8112134, -2.2800067, 15, "assets/images/wessex-icon.png"],
-      ["Wiltshire", 51.3069553, -1.9239737, 16, "assets/images/wessex-icon.png"],
-      ["Essex", 51.8208543, 0.5536933, 17, "assets/images/wessex-icon.png"],
-      ["London", 51.5268959, -0.3394719, 18, "assets/images/wessex-icon.png"],
-      ["Hertfordshire", 51.8024912, -0.2273691, 19, "assets/images/mercia-icon.png"],
-      ["Northamptonshire", 52.3326223, -0.8612687, 20, "assets/images/mercia-icon.png"],
-      ["Gloucestershire", 51.8812293, -2.1034647, 21, "assets/images/mercia-icon.png"],
-      ["Shropshire", 52.6422402, -2.6755677, 22, "assets/images/mercia-icon.png"],
-      ["Warwickshire", 52.2605613, -1.5047367, 23, "assets/images/mercia-icon.png"],
-      ["Oxfordshire", 51.7846263, -1.3165567, 24, "assets/images/mercia-icon.png"],
-      ["Staffordshire", 52.8558072, -2.0336547, 25, "assets/images/mercia-icon.png"],
-      ["Cambridgeshire", 52.3161863, 0.0763663, 26, "assets/images/mercia-icon.png"],
-      ["Cheshire", 53.2058542, -2.3560777, 27, "assets/images/mercia-icon.png"],
-      ["Derbyshire", 53.0718692, -1.5065807, 28, "assets/images/mercia-icon.png"],
-      ["Leicestershire", 52.6459512, -1.1054757, 29, "assets/images/mercia-icon.png"],
-      ["Herefordshire", 52.0949103, -2.6791307, 30, "assets/images/mercia-icon.png"],
-      ["Nottinghamshire", 53.1314412, -0.9775317, 31, "assets/images/mercia-icon.png"],
-      ["Lincolnshire", 53.1253312, -0.1814947,32, "assets/images/mercia-icon.png"],
-      ["Rutland", 52.6572112, -0.6478657, 33, "assets/images/mercia-icon.png"], 
-      ["Bedfordshire", 52.0671493, -0.4650577, 34, "assets/images/mercia-icon.png"],
-      ["Buckinghamshire", 51.7817283, -0.7855967, 35, "assets/images/mercia-icon.png"],
-      ["Berkshire", 51.4045883, -1.0161207, 36, "assets/images/mercia-icon.png"],
-    ];
-
-function setMarkers(map) {
-  const shape = {
-    coords: [1, 1, 1, 20, 18, 20, 18, 1],
-    type: "poly",
-  };
-
-  for (let i = 0; i < markers.length; i++) {
-    const marker = markers[i];
-    new google.maps.Marker({
-      position: { lat: marker[1], lng: marker[2] },
-      map,
-      icon: marker[4],
-      shape: shape,
-      title: marker[0],
-      zIndex: marker[3],
-    });
+    function emailSend(contactForm) {
+    emailjs.send("service_g5cggug","template_ht57zxe", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.feedback.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
 }
-}; 
