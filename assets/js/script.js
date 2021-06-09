73eeda6e56399c89b362dd52b1ede914dbbb060a
TJones305 <tim.w.j@hotmail.com>
2021-06-09 14:52:59 +0000

    added functionality to google map so when a flag is clicked it zooms into that area and recentres the map

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,63 +30,114 @@ function initMap() {
 
 //Co-ordinates and marker image for each county
 const markers = [
-        ["Suffolk", 52.1555477, 0.8305162, 1, "assets/images/eastanglia-icon.png"],
-        ["Norfolk", 52.6458602, 1.1001733, 2, "assets/images/eastanglia-icon.png"],
-        ["Lancashire", 53.8671211,-2.6202687, 3, "assets/images/northumbria-icon.png"],
-        ["Yorkshire", 53.9761501,-1.1075017, 4, "assets/images/northumbria-icon.png"],
-        ["Northumberland", 55.20421, -2.0282457, 5, "assets/images/northumbria-icon.png"],
-        ["Durham", 54.7109391, -1.7808717, 6, "assets/images/northumbria-icon.png"],
-        ["Cumbria", 54.6007491, -2.8466487, 7, "assets/images/northumbria-icon.png"],
-        ["Kent", 51.2108663, 0.7759013, 8, "assets/images/wessex-icon.png"],
-        ["Cornwall", 50.4087534, -4.8320077, 9, "assets/images/wessex-icon.png"],
-        ["Sussex", 50.933657,	-0.212399, 10, "assets/images/wessex-icon.png"],
-        ["Surrey", 51.2608723, -0.4978297, 11, "assets/images/wessex-icon.png"],
-        ["Hampshire", 51.1042083, -1.2324547, 12, "assets/images/wessex-icon.png"],
-        ["Somerset", 51.0893883, -2.8357007, 13, "assets/images/wessex-icon.png"],
-        ["Devon", 50.7888394, -3.7310407, 14, "assets/images/wessex-icon.png"],
-        ["Dorset", 50.8112134, -2.2800067, 15, "assets/images/wessex-icon.png"],
-        ["Wiltshire", 51.3069553, -1.9239737, 16, "assets/images/wessex-icon.png"],
-        ["Essex", 51.8208543, 0.5536933, 17, "assets/images/wessex-icon.png"],
-        ["London", 51.5268959, -0.3394719, 18, "assets/images/wessex-icon.png"],
-        ["Hertfordshire", 51.8024912, -0.2273691, 19, "assets/images/mercia-icon.png"],
-        ["Northamptonshire", 52.3326223, -0.8612687, 20, "assets/images/mercia-icon.png"],
-        ["Gloucestershire", 51.8812293, -2.1034647, 21, "assets/images/mercia-icon.png"],
-        ["Shropshire", 52.6422402, -2.6755677, 22, "assets/images/mercia-icon.png"],
-        ["Warwickshire", 52.2605613, -1.5047367, 23, "assets/images/mercia-icon.png"],
-        ["Oxfordshire", 51.7846263, -1.3165567, 24, "assets/images/mercia-icon.png"],
-        ["Staffordshire", 52.8558072, -2.0336547, 25, "assets/images/mercia-icon.png"],
-        ["Cambridgeshire", 52.3161863, 0.0763663, 26, "assets/images/mercia-icon.png"],
-        ["Cheshire", 53.2058542, -2.3560777, 27, "assets/images/mercia-icon.png"],
-        ["Derbyshire", 53.0718692, -1.5065807, 28, "assets/images/mercia-icon.png"],
-        ["Leicestershire", 52.6459512, -1.1054757, 29, "assets/images/mercia-icon.png"],
-        ["Herefordshire", 52.0949103, -2.6791307, 30, "assets/images/mercia-icon.png"],
-        ["Nottinghamshire", 53.1314412, -0.9775317, 31, "assets/images/mercia-icon.png"],
-        ["Lincolnshire", 53.1253312, -0.1814947,32, "assets/images/mercia-icon.png"],
-        ["Rutland", 52.6572112, -0.6478657, 33, "assets/images/mercia-icon.png"], 
-        ["Bedfordshire", 52.0671493, -0.4650577, 34, "assets/images/mercia-icon.png"],
-        ["Buckinghamshire", 51.7817283, -0.7855967, 35, "assets/images/mercia-icon.png"],
-        ["Berkshire", 51.4045883, -1.0161207, 36, "assets/images/mercia-icon.png"],
+        ["Suffolk", 52.1555477, 0.8305162, "assets/images/eastanglia-icon.png", "east anglia"],
+        ["Norfolk", 52.6458602, 1.1001733, "assets/images/eastanglia-icon.png", "east anglia"],
+        ["Lancashire", 53.8671211,-2.6202687, "assets/images/northumbria-icon.png", "northumbria"],
+        ["Yorkshire", 53.9761501,-1.1075017, "assets/images/northumbria-icon.png", "northumbria"],
+        ["Northumberland", 55.20421, -2.0282457, "assets/images/northumbria-icon.png", "northumbria"],
+        ["Durham", 54.7109391, -1.7808717, "assets/images/northumbria-icon.png", "northumbria"],
+        ["Cumbria", 54.6007491, -2.8466487, "assets/images/northumbria-icon.png", "northumbria"],
+        ["Kent", 51.2108663, 0.7759013, "assets/images/wessex-icon.png", "wessex"],
+        ["Cornwall", 50.4087534, -4.8320077, "assets/images/wessex-icon.png", "wessex"],
+        ["Sussex", 50.933657,	-0.212399, "assets/images/wessex-icon.png", "wessex"],
+        ["Surrey", 51.2608723, -0.4978297, "assets/images/wessex-icon.png", "wessex"],
+        ["Hampshire", 51.1042083, -1.2324547, "assets/images/wessex-icon.png", "wessex"],
+        ["Somerset", 51.0893883, -2.8357007, "assets/images/wessex-icon.png", "wessex"],
+        ["Devon", 50.7888394, -3.7310407, "assets/images/wessex-icon.png", "wessex"],
+        ["Dorset", 50.8112134, -2.2800067, "assets/images/wessex-icon.png", "wessex"],
+        ["Wiltshire", 51.3069553, -1.9239737, "assets/images/wessex-icon.png", "wessex"],
+        ["Essex", 51.8208543, 0.5536933, "assets/images/wessex-icon.png", "wessex"],
+        ["London", 51.5268959, -0.3394719, "assets/images/wessex-icon.png", "wessex"],
+        ["Hertfordshire", 51.8024912, -0.2273691, "assets/images/mercia-icon.png", "mercia"],
+        ["Northamptonshire", 52.3326223, -0.8612687, "assets/images/mercia-icon.png", "mercia"],
+        ["Gloucestershire", 51.8812293, -2.1034647, "assets/images/mercia-icon.png", "mercia"],
+        ["Shropshire", 52.6422402, -2.6755677, "assets/images/mercia-icon.png", "mercia"],
+        ["Warwickshire", 52.2605613, -1.5047367, "assets/images/mercia-icon.png", "mercia"],
+        ["Oxfordshire", 51.7846263, -1.3165567, "assets/images/mercia-icon.png", "mercia"],
+        ["Staffordshire", 52.8558072, -2.0336547, "assets/images/mercia-icon.png", "mercia"],
+        ["Cambridgeshire", 52.3161863, 0.0763663, "assets/images/mercia-icon.png", "mercia"],
+        ["Cheshire", 53.2058542, -2.3560777, "assets/images/mercia-icon.png", "mercia"],
+        ["Derbyshire", 53.0718692, -1.5065807, "assets/images/mercia-icon.png", "mercia"],
+        ["Leicestershire", 52.6459512, -1.1054757, "assets/images/mercia-icon.png", "mercia"],
+        ["Herefordshire", 52.0949103, -2.6791307, "assets/images/mercia-icon.png", "mercia"],
+        ["Nottinghamshire", 53.1314412, -0.9775317, "assets/images/mercia-icon.png", "mercia"],
+        ["Lincolnshire", 53.1253312, -0.1814947, "assets/images/mercia-icon.png", "mercia"],
+        ["Rutland", 52.6572112, -0.6478657, "assets/images/mercia-icon.png", "mercia"], 
+        ["Bedfordshire", 52.0671493, -0.4650577, "assets/images/mercia-icon.png", "mercia"],
+        ["Buckinghamshire", 51.7817283, -0.7855967, "assets/images/mercia-icon.png", "mercia"],
+        ["Berkshire", 51.4045883, -1.0161207, "assets/images/mercia-icon.png", "mercia"],
         ];
+
+// Working Code:
+   /*
+        const gmarker = new google.maps.Marker({
+            position: {lat: marker[1], lng: marker[2] },
+            map,
+            title: "Click to zoom",
+            icon: marker[3],
+            shape: shape,
+            title: marker[0],
+        });
+
+        map.addListener("center_changed", () => {
+            window.setTimeout(() => {
+                map.panTo(gmarker.getPosition());
+            }, 3000);
+        });
+
+        gmarker.addListener("click", () => {
+            map.setZoom(10);
+            map.setCenter(gmarker.getPosition());
+        }); */
+     
 
 //Assigns marker positions and image
 function setMarkers(map) {
+
     const shape = {
         coords: [1, 1, 1, 20, 18, 20, 18, 1],
         type: "poly",
-  };
+    };
 
-  for (let i = 0; i < markers.length; i++) {
-        const marker = markers[i];
-        new google.maps.Marker({
-        position: { lat: marker[1], lng: marker[2] },
-        map,
-        icon: marker[4],
-        shape: shape,
-        title: marker[0],
-        zIndex: marker[3],
-    });
-  }
+    for (let i = 0; i < markers.length; i++) {
+        const county = markers[i];
+        var category = markers[i][4];
+
+        let marker = new google.maps.Marker({
+            position: { lat: county[1], lng: county[2] },
+            map,
+            title: "Click to zoom",
+            icon: county[3],
+            shape: shape,
+            title: county[0],
+        });
+
+        marker.addListener("click", () => {
+
+            window.setTimeout(() => {
+            map.setZoom(9);
+            map.setCenter(marker.getPosition());
+        }, 500);
+        });
+    }
 }
+
+/*
+// Filter markers on map
+filterMarkers = function (category) {
+    for (i = 0; i < markers.length; i++) {
+        filtered = marker[i];
+        //if same category or category not picked
+        if (filtered.category == category || category.length === 0) {
+            filtered.setVisible(true);
+        }
+        //Categories don't match
+        else {
+            filtered.setVisible(false);
+        }
+    }
+}
+*/
 
 //Changes color on accordion buttons when mouseenters and reverts when mouseleaves. On non-desktop colour remains until next tap.
 function colorChange(event) {
